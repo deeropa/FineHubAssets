e1cc5c88c1fac77be8eb535cb4e2ccc0a2648acd
deeropa <idiotasdomas@gmail.com>
2025-06-14 18:03:25 +0100

    Update ColorsModule.lua

--- ColorsModule.lua
+++ ColorsModule.lua
@@ -1,36 +1,33 @@
 local ColorsModule = {}
 
 function ColorsModule.createColorPickers(Tabs)
-    local UltraColor = Tabs.EspTab:AddColorpicker("UltraColor", {
+    local colors = {}
+
+    colors.UltraColor = Tabs.EspTab:AddColorpicker("UltraColor", {
         Title = "Ultra ESP Color",
         Default = Color3.fromRGB(255, 0, 0)
     })
-    UltraColor:SetValueRGB(Color3.fromRGB(255, 0, 0))
+    colors.UltraColor:SetValueRGB(Color3.fromRGB(255, 0, 0))
 
-    local IllusionistColor = Tabs.EspTab:AddColorpicker("IllusionistColor", {
+    colors.IllusionistColor = Tabs.EspTab:AddColorpicker("IllusionistColor", {
         Title = "Illusionist Color",
         Default = Color3.fromRGB(0, 170, 255)
     })
-    IllusionistColor:SetValueRGB(Color3.fromRGB(0, 170, 255))
+    colors.IllusionistColor:SetValueRGB(Color3.fromRGB(0, 170, 255))
 
-    local NoUltraColor = Tabs.EspTab:AddColorpicker("NoUltraColor", {
+    colors.NoUltraColor = Tabs.EspTab:AddColorpicker("NoUltraColor", {
         Title = "No Ultra Color",
         Default = Color3.fromRGB(255, 255, 255)
     })
-    NoUltraColor:SetValueRGB(Color3.fromRGB(255, 255, 255))
+    colors.NoUltraColor:SetValueRGB(Color3.fromRGB(255, 255, 255))
 
-    local YourselfColor = Tabs.EspTab:AddColorpicker("YourselfColor", {
+    colors.YourselfColor = Tabs.EspTab:AddColorpicker("YourselfColor", {
         Title = "Yourself Color",
         Default = Color3.fromRGB(0, 255, 0)
     })
-    YourselfColor:SetValueRGB(Color3.fromRGB(0, 255, 0))
+    colors.YourselfColor:SetValueRGB(Color3.fromRGB(0, 255, 0))
 
-    return {
-        UltraColor = UltraColor,
-        IllusionistColor = IllusionistColor,
-        NoUltraColor = NoUltraColor,
-        YourselfColor = YourselfColor
-    }
+    return colors
 end
 
 return ColorsModule
